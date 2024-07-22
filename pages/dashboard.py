bce45377e8247cf759de9d0ffe603d626f72ef56
ajeong <ajung7038@naver.com>
2024-07-23 00:17:24 +0900

    Feat: add dashboard #4

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -4,6 +4,10 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from pages import videoPage
 import pandas as pd
 import numpy as np
+import plotly.express as px
+
+st.set_page_config(layout="wide")
+empty1,con1,empty2 = st.columns([0.2,0.8,0.2])
 
 # 대시보드
 st.sidebar.page_link("main.py", label="실시간", icon = "🚨")
@@ -16,11 +20,38 @@ image = "assets/switcessLogo.png"
 st.logo(image, link="https://github.com/switcess/switchOn_Frontend")
 
 
-# 월 별 이상행동 감지 그래프
-st.subheader("월 별 이상행동 감지 그래프")
+# 이상행동 감지 그래프
+st.title("이상행동 감지 그래프")
+
+st.subheader("시간대")
 chart_data = pd.DataFrame(np.random.randn(10, 1))
+
 st.line_chart(chart_data)
 
 
+col1, col2 = st.columns([1, 1])
 
 
+chart_data = pd.DataFrame({
+    'crime': ['절도', '방화', '도난', '전도'],
+    'time': [1, 3, 5, 4]
+})
+
+with col1:
+    st.subheader("월 별")
+    fig = px.bar(chart_data, x='crime', y='time',
+	text_auto=True) # text_auto: 값 표시 여부
+    # fig.update_layout(width=800, height=600)
+    # fig.update_traces(textangle=0) # 그래프 안에 텍스트 씌우기
+    st.plotly_chart(fig)
+
+
+with col2:
+    st.subheader("파이")
+    fig = px.pie(chart_data, names='crime', values='time', hole=.3) # hole을 주면 donut 차트
+
+    fig.update_traces(textposition='inside', textinfo='percent+label+value')
+    fig.update_layout(font=dict(size=14))
+    fig.update(layout_showlegend=False) # 범례표시 제거
+
+    st.plotly_chart(fig)
